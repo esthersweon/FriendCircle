a93358952f43ef250b16272d70d78a452fee14a4
Esther S Weon <esther.s.weon@gmail.com>
2014-06-02 17:23:17 -0700

    finish all post and circle views and links navigating between them

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -14,31 +14,23 @@ class CirclesController < ApplicationController
   def create
     @circle = current_user.circles.new(circle_params)
 
-    # @circle = Circle.new(user_id: current_user.id)
-    # fail
     if @circle.save
-      # circle_params.each do |id|
-      #   membership = CircleMembership.new(
-      #     member_id: id.to_i,
-      #     circle_id: @circle.id)
-      #   membership.save
-      # end
-      redirect_to user_posts_url
+      redirect_to circle_url(@circle)
     else
       @users = User.all
       flash.now[:errors] = @circle.errors.full_messages
       render :new
     end
 
-    # p circle_params
-  #   p params
-  #
-  #   redirect_to new_circle_url
+    def show
+      @circle = Circle.find(params[:id])
+      render :show
+    end
   end
 
   private
 
   def circle_params
-    params.require(:circle).permit(:title, :member_ids => [])
+    params.require(:circle).permit(:title, :user_id, :member_ids => [])
   end
 end
